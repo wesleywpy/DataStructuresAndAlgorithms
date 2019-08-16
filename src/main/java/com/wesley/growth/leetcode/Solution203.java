91af1f7c7569dc5dafe47292bec4c83a999c5042
yani <wangp_yong@163.com>
2019-08-16 15:20:15 +0800

    lettcode 21 迭代实现

--- src/main/java/com/wesley/growth/leetcode/Solution203.java
+++ src/main/java/com/wesley/growth/leetcode/Solution203.java
@@ -93,37 +93,3 @@ public class Solution203 {
     }
 
 }
-
- class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) {
-         val = x;
-     }
-
-     ListNode(int[] arr) {
-         if (arr == null || arr.length < 1) {
-             throw new IllegalArgumentException("参数不能错误");
-         }
-
-         this.val = arr[0];
-         ListNode cur = this;
-         for (int i = 1; i < arr.length; i ++) {
-             cur.next = new ListNode(arr[i]);
-             cur = cur.next;
-         }
-     }
-
-     @Override
-     public String toString() {
-         StringBuilder res = new StringBuilder();
-         ListNode cur = this;
-         while (cur != null) {
-             res.append(cur.val).append("->");
-             cur = cur.next;
-         }
-
-         res.append("NULL");
-         return res.toString();
-     }
- }
--- src/main/java/com/wesley/growth/leetcode/Solution21.java
+++ src/main/java/com/wesley/growth/leetcode/Solution21.java
@@ -13,20 +13,47 @@ package com.wesley.growth.leetcode;
  */
 public class Solution21 {
 
-    public ListNode mergeTwoLists(ListNode node1, ListNode node2) {
-
+    /**
+     * 迭代实现
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummyNode = new ListNode(-1);
-        dummyNode.next = node1;
 
         ListNode prev = dummyNode;
-        while (prev.next != null) {
-
-
-
+        while (l1 != null && l2 != null) {
+            // 如果 l1 当前位置的值小于等于 l2 ，我们就把 l1 的值接在 prev 节点的后面同时将 l1 指针往后移一个
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                // 否则，我们对 l2 做同样的操作。
+                prev.next = l2;
+                l2 = l2.next;
+            }
             prev = prev.next;
         }
 
-        return null;
+        /**
+         * 在循环终止的时候， l1 和 l2 至多有一个是非空的。
+         * 由于输入的两个链表都是有序的，所以不管哪个链表是非空的，它包含的所有元素都比前面已经合并链表中的所有元素都要大。
+         * 这意味着我们只需要简单地将非空链表接在合并链表的后面，并返回合并链表。
+         */
+        prev.next = l1 != null ? l1 : l2;
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 4};
+        int[] arr2 = {1, 3, 4};
+        ListNode node1 = new ListNode(arr1);
+        ListNode node2 = new ListNode(arr2);
+
+        ListNode result = new Solution21().mergeTwoLists(node1, node2);
+        System.out.println(result);
     }
 
 }
