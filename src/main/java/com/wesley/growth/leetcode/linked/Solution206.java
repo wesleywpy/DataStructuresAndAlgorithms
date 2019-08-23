e96b1b733a65fbc6ea214a1e870fe135b4956ee6
yani <wangp_yong@163.com>
2019-08-23 11:36:14 +0800

    反转单向链表

---
+++ src/main/java/com/wesley/growth/leetcode/linked/Solution206.java
@@ -0,0 +1,85 @@
+package com.wesley.growth.leetcode.linked;
+
+/**
+ * <p>
+ * 反转一个单链表
+ * 示例:
+ *  输入: 1->2->3->4->5->NULL
+ *  输出: 5->4->3->2->1->NULL
+ * </p>
+ * Email yani@uoko.com
+ *
+ * @author Created by Yani on 2019/08/22
+ */
+public class Solution206 {
+
+    /**
+     * 反转一个链表, 迭代实现
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            // 临时节点，暂存当前节点的下一节点，用于后移
+            ListNode next = curr.next;
+            // 将当前节点指向它前面的节点
+            curr.next = prev;
+            // 前指针后移
+            prev = curr;
+            // 当前指针后移
+            curr = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 递归实现, 与迭代实现 逻辑相似
+     */
+    public ListNode reverseListByRecursion(ListNode head) {
+        return reverse(null, head, 0);
+    }
+
+    private ListNode reverse(ListNode pre, ListNode cur){
+        if(cur == null) {
+            return pre;
+        }
+        ListNode next = cur.next;
+        cur.next = pre;
+        return reverse(cur,next);
+    }
+
+    private ListNode reverse(ListNode pre, ListNode cur, int depth){
+        if(cur == null) {
+            System.out.println(generateDepthString(depth) + "Pre : "+ pre);
+            return pre;
+        }
+
+        System.out.println(generateDepthString(depth) + "Pre : "+ pre);
+        System.out.println(generateDepthString(depth) + "Cur : "+ cur);
+        // 暂存当前节点的下一节点，用于继续递归
+        ListNode next = cur.next;
+        // 当前节点 指向前一个节点
+        cur.next = pre;
+        // 当前节点作为前一个节点, 进行递归
+        return reverse(cur, next, depth + 1);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder sbl = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            sbl.append("-- ");
+        }
+
+        return sbl.toString();
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        ListNode node = new ListNode(arr1);
+        ListNode result = new Solution206().reverseListByRecursion(node);
+        System.out.println(result);
+    }
+
+}
+
